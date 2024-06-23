68cd5a6356506859ec61f7097bb7505593401d9d
Denis <smutok.denis@gmail.com>
2024-06-24 00:19:09 +0300

    прошел ввод/вывод

---
+++ Задания/Print.py
@@ -0,0 +1,65 @@
+# star="*"
+# for i in range (1,8):
+#     print(star*i)
+
+# name= input()
+# print("Привет,", name)
+
+# a = input()
+# b = input()
+# c = input()
+# print(a,b,c, sep="\n")
+
+# print('a', 'b', 'c', sep='*')
+# print('d', 'e', 'f', sep='**', end='')
+# print('g', 'h', 'i', sep='+', end='%')
+# print('j', 'k', 'l', sep='-', end='\n')
+# print('m', 'n', 'o', sep='/', end='!')
+# print('p', 'q', 'r', sep='1', end='%')
+# print('s', 't', 'u', sep='&', end='\n')
+# print('v', 'w', 'x', sep='%')
+# print('y', 'z', sep='/', end='!')
+
+# sep = input()
+# a = input()
+# b = input()
+# c = input()
+# print(a,b,c, sep=sep)
+
+# x = 3
+# y = 4
+# z = x + y
+# z = z + 1
+# x = y
+# y = 5
+# x = z + y + 7
+#
+# print(x)
+
+# n=int(input())
+# for i in range(n,n+3):
+#     print(i)
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# print(a+b+c)
+
+# a=int(input())
+# print("Объем =", a*a*a)
+# print("Площадь полной поверхности =", 6*a*a)
+
+# a=int(input())
+# b=int(input())
+# print(3*(a+b)*(a+b)*(a+b)+275*b*b-127*a-41)
+
+# a=int(input())
+# print("Следующее за числом", a, "число:", a+1)
+# print("Для числа", a, "предыдущее число:", a-1)
+
+# a = 15 // (16 % 7)
+# b = 34 % a * 5 - 29 % 5 * 2
+# print(a + b)
+
+a = 82 // 3 ** 2 % 7
+print(a)
